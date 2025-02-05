87cb290ff61defd792f76ab14321842a61822fcc
Nestor <nestorfumero2005@gmail.com>
2025-02-05 10:50:49 -0500

    arreglando audio

--- obj/Debug/net8.0/Proyecto.AssemblyInfo.cs
+++ obj/Debug/net8.0/Proyecto.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Proyecto")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2062b26e8efa7fd028d4560a953b780d00664f9f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+54cf49cca7594fa8f4b585076d05406f06f0f0f1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Proyecto")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Proyecto")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
